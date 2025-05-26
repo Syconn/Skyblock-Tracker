a6e592dfc3b1cc070e00840e8b5d6a871af38fe8
Syconn <49461127+Syconn@users.noreply.github.com>
2025-05-26 03:31:39 -0400

    New Flip for NamesToID

--- src/Pages/Items.tsx
+++ src/Pages/Items.tsx
@@ -1,27 +1,28 @@
 interface Skin {
-  value: string;
-  signature: string;
+	value: string;
+	signature: string;
 }
 
 interface Item {
-  material: string;
-  durability: number;
-  skin: Skin;
-  name: string;
-  category: string;
-  tier: string;
-  npc_sell_price: number;
-  id: string;
+	material: string;
+	durability: number;
+	skin: Skin;
+	name: string;
+	category: string;
+	tier: string;
+	npc_sell_price: number;
+	id: string;
 }
 
 export type ItemsResponse = {
-  success: boolean;
-  lastUpdated: number;
-  items: Item[];
+	success: boolean;
+	lastUpdated: number;
+	items: Item[];
 }
 
-export function nameRecord(response: ItemsResponse | null): Record<string, string> {
+export function nameRecord(response: ItemsResponse | null, flip: boolean): Record<string, string> {
     const items: { [id: string]: string } = {};
-    if (response?.items) response?.items.forEach(item => items[item.name.toLowerCase()] = item.id);
+    if (response?.items && flip) response?.items.forEach(item => items[item.id] = item.name.toLowerCase());
+	if (response?.items && !flip) response?.items.forEach(item => items[item.name.toLowerCase()] = item.id);
     return items;
 }
